c429dbadeda68073bd4c872220a1a73bc4602886
Beijing Chen <nbutimage@126.com>
2023-06-20 15:10:33 +0800

    Update input.py

--- input.py
+++ input.py
@@ -2,11 +2,31 @@ import torch.nn as nn
 import torch
 import torch.quantization
 import numpy as np
-from Crypto.Cipher import
-from pybloomfilter import BloomFilter
+from PIL import Image
+from key import key
+# 定义混沌系统的参数
+a = 10
+b = 8 / 3
+r = 28
 
-# 加密密钥，必须是长度为 16、24 或 32 的字符串
-key = '0123456789abcdef'
+# 定义初始状态
+x0 = 0.1
+y0 = 0.2
+z0 = 0.3
+
+# 定义迭代次数
+n = 100000
+
+
+
+# 定义加密函数
+def encrypt(image, key):
+    plaintext = np.array(image).flatten()
+    binary_plaintext = np.unpackbits(plaintext)
+    binary_ciphertext = np.bitwise_xor(binary_plaintext, key)
+    ciphertext = np.packbits(binary_ciphertext)
+    ciphertext_image = Image.fromarray(ciphertext.reshape(image.shape))
+    return ciphertext_image
 
 
 
@@ -27,8 +47,9 @@ def readImg(image):
     image_1 = randomImage(image)
     image_2 = image - image_1
     ## 对其进行加密
-    image_1 = encrypt_image(image_1)
-    image_2 = encrypt_image(image_2)
+
+    image_1 = encrypt(image_1, key)
+    image_2 = encrypt(image_2, key)
 
     image_1 = torch.tensor(image_1,dtype=torch.float32)
     image_2 = torch.tensor(image_2, dtype=torch.float32)
@@ -38,41 +59,6 @@ def readImg(image):
 
     return image_1, image_2
 
-def encrypt_image(image):
-    # 将图像分解并加密每个通道的分解结果
-    encrypted_channels = []
-    for c in range(3):
-        # 创建一个 AES 加密器
-        cipher = AES.new(key.encode(), AES.MODE_ECB)
-        # 将当前通道的分解结果转换为 bytes 类型
-        data = bytes(np.ndarray.flatten(image[:,:,c]))
-        # 对分解结果进行加密
-        encrypted_data = cipher.encrypt(data)
-        # 将加密后的结果保存到列表中
-        encrypted_channels.append(encrypted_data)
-    # 将加密后的结果合并成一个加密后的彩色图像
-    encrypted_image = np.zeros(image.shape, np.uint8)
-    for c in range(3):
-        # 获取图像的宽和高
-        h, w = c.shape
-        data = ''.join([str(c[i][j]) for i in range(h) for j in range(w)])
-        # 创建一个 Bloom Filter，用于保存图像的哈希值
-        bf = BloomFilter(capacity=1000000, error_rate=0.001)
-        # 将图像的哈希值添加到 Bloom Filter 中
-        bf.add(data)
-        # 将 Bloom Filter 转换为 bytes 类型
-        bf_bytes = bf.tobytes()
-        # 创建一个 AES 解密器
-        cipher = AES.new(key.encode(), AES.MODE_ECB)
-        # 对当前通道的加密结果进行解密
-        decrypted_data = cipher.decrypt(bf_bytes)
-        # 将解密后的结果转换为 numpy 数组
-        decrypted_array = np.frombuffer(decrypted_data, np.uint8)
-        # 将解密后的结果重构为原始图像的形状
-        decrypted_channel = np.reshape(decrypted_array, (image.shape[0], image.shape[1]))
-        # 将解密后的结果赋值给加密后的图像的对应通道
-        encrypted_image[:,:,c] = decrypted_channel
-    return encrypted_image
 
 if __name__ == '__main__':
     print(-1.2433e+01+1.5746)
